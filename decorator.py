49b01137a659f451a1a0325a14be1b125256068a
Tom Victor <vjtomvictor@icloud.com>
2019-12-15 07:58:01 +0530

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,16 +1,24 @@
 
 
 
-def smart_divide(func):
-   def inner(a,b):
-      print("I am going to divide",a,"and",b)
-      if b == 0:
-         print("Whoops! cannot divide")
-         return
+# def smart_divide(func):
+#    def inner(a,b):
+#       print("I am going to divide",a,"and",b)
+#       if b == 0:
+#          print("Whoops! cannot divide")
+#          return
 
-      return func(a,b)
-   return inner
+#       return func(a,b)
+#    return inner
+
+def smart_divide(func):
+    def inner(*args, **kwargs):
+        print("I can decorate any function")
+        return func(*args, **kwargs)
+    return inner
 
 @smart_divide
 def divide(a,b):
     return a/b
+
+print(divide(1,5))
